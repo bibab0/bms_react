618d2e7501cbed92026ae447b23d9dd5f1039c02
panshihao <853297713@qq.com>
2020-10-14 11:02:50 +0800

    *侧边栏收缩展开功能

--- src/views/Layout/SiderBar.js
+++ src/views/Layout/SiderBar.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import { Layout, Menu } from 'antd';
-import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { AppstoreOutlined, MailOutlined, SettingOutlined, MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
+import './SiderBar.css';
+import reactLogo from '../../assets/images/reactLogo.svg';
 const { SubMenu } = Menu;
 const { Sider } = Layout;
 class SiderBar extends Component {
+    state = {
+        collapsed: false
+    };
+    toggleCollapsed = () => {
+        this.setState({
+            collapsed: !this.state.collapsed
+        })
+    }
     handleClick = e => {
         console.log('click ', e);
     };
     render() {
         return (
-            <Sider>
+            <Sider collapsed={this.state.collapsed} collapsedWidth="48">
+                <div className="logoBox">
+                    <img src={reactLogo} alt="logo" />
+                    {this.state.collapsed ? '' : <span>BMS-REACT</span>}
+                </div>
                 <Menu
+                    className="menuBox"
                     onClick={this.handleClick}
                     mode="inline"
                     theme="dark"
@@ -46,6 +61,9 @@ class SiderBar extends Component {
                         <Menu.Item>Option 12</Menu.Item>
                     </SubMenu>
                 </Menu>
+                <div className="collapseBox" onClick={this.toggleCollapsed}>
+                    {this.state.collapsed ? <MenuUnfoldOutlined className="collapseIcon" /> : <MenuFoldOutlined className="collapseIcon" />}
+                </div>
             </Sider>
         );
     }
